a129cb3e46888a24d195fc3cda4569b3d7cdc72c
ansthsys <azmiythufail91@gmail.com>
2023-04-08 21:32:59 +0700

    Add method index for handling view

--- app/Controllers/PartnerController.php
+++ app/Controllers/PartnerController.php
@@ -7,4 +7,20 @@ use App\Models\PartnerModel;
 
 class PartnerController extends BaseController
 {
+    protected $partner;
+    protected $helpers = ['form', 'filesystem'];
+
+    public function __construct()
+    {
+        $this->partner = new PartnerModel();
+    }
+
+    public function index()
+    {
+        $data = $this->partner->findAll();
+
+        return view('admin/partner/index', [
+            'data' => $data
+        ]);
+    }
 }
